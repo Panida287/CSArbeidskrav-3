aee483b47215d99f184777a83c12d12e33d96833
LailaElisabeth <laila_elisabeth@hotmail.com>
2026-04-19 14:33:09 +0200

    feat: implemented UserRespository, Register, Login, Logout.

--- Arbeidskrav 3/EventPlatform/EventPlatform/Services/UserService.cs
+++ Arbeidskrav 3/EventPlatform/EventPlatform/Services/UserService.cs
@@ -1,4 +1,6 @@
 using EventPlatform.Models;
+using EventPlatform.Repositories;
+using EventPlatform.Utilities;
 
 namespace EventPlatform.Services;
 
@@ -9,15 +11,49 @@ namespace EventPlatform.Services;
 public class UserService
 {
     private User? _currentUser;
+    private readonly UserRepository _userRepository;
+
+    public UserService(UserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
 
     /// <summary>Registers a new user. Returns false if username is taken.</summary>
-    public bool Register(string username, string password) => throw new NotImplementedException();
+    public bool Register(string username, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Username and password cannot be empty.");
+
+        if (_userRepository.Exists(username))
+            throw new InvalidOperationException("A user with that username already exists.");
+
+        string hashedPassword = PasswordHelper.Hash(password);
+        var user = new User { Username = username, PasswordHash = hashedPassword};
+        _userRepository.Insert(user);
+        return true;
+
+    }
 
     /// <summary>Logs in a user. Returns false if credentials are invalid.</summary>
-    public bool Login(string username, string password) => throw new NotImplementedException();
+    public bool Login(string username, string password)
+    {
+        if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Username and password cannot be empty.");
+
+        var user = _userRepository.FindByUsername(username);
+
+        if (user == null || !PasswordHelper.Verify(password, user.PasswordHash))
+            throw new UnauthorizedAccessException("Invalid username or password.");
+
+        _currentUser = user;
+        return true;
+    }
 
     /// <summary>Logs out the current user.</summary>
-    public void Logout() => throw new NotImplementedException();
+    public void Logout()
+    {
+        _currentUser = null;
+    }
 
     /// <summary>Returns the currently logged-in user, or null if not logged in.</summary>
     public User? GetCurrentUser() => _currentUser;
