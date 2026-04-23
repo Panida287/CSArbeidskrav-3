95abd443168070a06965373bbd1d346e4d6e271d
LailaElisabeth <laila_elisabeth@hotmail.com>
2026-04-23 18:11:49 +0200

    AuthMenu.cs Show()

--- Arbeidskrav 3/EventPlatform/EventPlatform/UI/Menus/AuthMenu.cs
+++ Arbeidskrav 3/EventPlatform/EventPlatform/UI/Menus/AuthMenu.cs
@@ -14,7 +14,34 @@ public class AuthMenu
         _userService = userService;
     }
 
-    public void Show() => throw new NotImplementedException();
+    public void Show()
+    {
+        while (true)
+        {
+            ConsoleHelper.ClearAndPrintHeader("Welcome to EventPlatform!");
+            ConsoleHelper.PrintDivider();
+            Console.WriteLine(" 1. Register");
+            Console.WriteLine(" 2. Log In");
+            Console.WriteLine(" 3. Exit");
+            ConsoleHelper.PrintDivider();
+            Console.WriteLine("Choose an option: ");
+
+            string choice = Console.ReadLine() ?? "";
+
+            switch (choice)
+            {
+                case "1": ShowRegister(); break;
+                case "2": ShowLogin(); break;
+                case "3": return;
+                default:
+                    ConsoleHelper.PrintError("Invalid option. Please try again.");
+                    ConsoleHelper.PressAnyKeyToContinue();
+                    break;
+            }
+        }
+    }
+    
+    
     public void ShowRegister() => throw new NotImplementedException();
     public void ShowLogin() => throw new NotImplementedException();
 }
