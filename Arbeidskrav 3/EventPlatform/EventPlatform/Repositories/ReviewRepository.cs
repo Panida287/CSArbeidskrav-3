0b1af7cdb19cec7382b68408ffa4021aa57b07e2
xghrgmax2089-web <xghrgmax2089@gmail.com>
2026-04-28 17:30:04 +0200

    feat: Implement ReviewRepository methods
    Implemented Insert, GetByEvent and ExistsForBooking in ReviewRepository

--- Arbeidskrav 3/EventPlatform/EventPlatform/Repositories/ReviewRepository.cs
+++ Arbeidskrav 3/EventPlatform/EventPlatform/Repositories/ReviewRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.Sqlite;
 using EventPlatform.Models;
 
 namespace EventPlatform.Repositories;
@@ -8,11 +9,74 @@ namespace EventPlatform.Repositories;
 public class ReviewRepository
 {
     /// <summary>Inserts a new review. Returns the new ID.</summary>
-    public int Insert(Review review) => throw new NotImplementedException();
+    public int Insert(Review review)
+    {
+        using var connection = new SqliteConnection("Data Source=eventplatform.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            INSERT INTO REVIEWS (BookingId, EventId, UserId, Rating, Comment)
+            VALUES (@bookingId, @eventId, @userId, @rating, @comment);
+
+            SELECT last_insert_rowid();
+";
+        command.Parameters.AddWithValue("@bookingId", review.BookingId);
+        command.Parameters.AddWithValue("@eventId", review.EventId);
+        command.Parameters.AddWithValue("@userId", review.UserId);
+        command.Parameters.AddWithValue("@rating", review.Rating);
+        command.Parameters.AddWithValue("@comment", review.Comment);
+
+        var result = command.ExecuteScalar();
+        return Convert.ToInt32(result);
+    }
 
     /// <summary>Returns all reviews for the given event.</summary>
-    public List<Review> GetByEvent(int eventId) => throw new NotImplementedException();
+    public List<Review> GetByEvent(int eventId)
+    {
+        var reviews = new List<Review>();
+        
+        using var connection = new SqliteConnection("Data Source = eventplatform.db");
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            SELECT * FROM Reviews 
+            WHERE EventId = @eventId;
+";
+        command.Parameters.AddWithValue("@eventId", eventId);
+
+        using var reader = command.ExecuteReader();
+        
+        While (reader.Read())
+        {
+            reviews.Add(new Review
+            {
+                Id = reader.GetInt32(0),
+                BookingId = reader.GetInt32(1),
+                EventId = reader.GetInt32(2),
+                UserId = reader.GetInt32(3),
+                Rating = reader.GetInt32(4),
+                Comment = reader.IsDBNull(5) ? "" : reader.GetString(5)
+            });
+        }
+        return reviews;
+    }
 
     /// <summary>Returns true if a review already exists for the given booking.</summary>
-    public bool ExistsForBooking(int bookingId) => throw new NotImplementedException();
+    public bool ExistsForBooking(int bookingId)
+    {
+        using var connection = new SqliteConnection("Data Source = eventplatform.db");
+        connection.Open();
+        
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            SELECT COUNT(*) FROM Reviews 
+            WHERE BookingId = @bookingId; 
+        ";
+        command.Parameters.AddWithValue("@bookingId", bookingId);
+
+        var count = Convert.ToInt32(command.ExecuteScalar());
+        return count > 0;
+    }
 }
